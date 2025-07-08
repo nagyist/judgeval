c858bb40906c4f5cbd90ee7c135172c43c9a9238
Aaryan Divate <divateaaryan@gmail.com>
2025-07-07 17:56:12 -0700

    Deleted duplicate script

--- src/update_types.sh
+++ src/update_types.sh
@@ -2,7 +2,7 @@
 
 # Make sure Judgment backend server is running on port 8000
 # This openapi_transform.py will get the relevant parts of the openapi.json file and save it to openapi_new.json
-uv run judgeval/data/openapi_transform.py > judgeval/data/openapi_new.json
+uv run judgeval/data/scripts/openapi_transform.py > judgeval/data/openapi_new.json
 
 # Then, datamodel-codegen will generate the judgment_types.py file based on the schema in openapi_new.json.
 datamodel-codegen --input judgeval/data/openapi_new.json --output judgeval/data/judgment_types.py --use-annotated
