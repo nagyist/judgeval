3b876fb109a24595efeb757fb524274bd731af7a
Mandolaro <minhquangpham6@gmail.com>
2025-07-15 20:11:48 -0700

    fix unit test

--- src/judgeval/common/tracer/__init__.py
+++ src/judgeval/common/tracer/__init__.py
@@ -1,31 +1,23 @@
 from judgeval.common.tracer.core import (
-    DeepTracer,
     TraceClient,
     _DeepTracer,
-    trace_judgment_run,
-    trace_judgment_run_sync,
-    trace_score_run,
-    trace_score_run_sync,
-    transform_to_trace_span,
-    transform_to_trace_span_sync,
+    Tracer,
+    wrap,
+    current_span_var,
+    current_trace_var,
 )
-from judgeval.common.tracer.otel_exporter import JudgevalOTLPSpanExporter
-from judgeval.common.tracer.otel_span_processor import JudgevalBatchSpanProcessor
-from judgeval.common.tracer.span_transformer import create_trace_span_from_otel_span
+from judgeval.common.tracer.otel_exporter import JudgmentAPISpanExporter
+from judgeval.common.tracer.otel_span_processor import JudgmentSpanProcessor
 from judgeval.common.tracer.trace_manager import TraceManagerClient
 
 __all__ = [
-    "DeepTracer",
     "_DeepTracer",
     "TraceClient",
-    "trace_judgment_run",
-    "trace_judgment_run_sync",
-    "trace_score_run",
-    "trace_score_run_sync",
-    "transform_to_trace_span",
-    "transform_to_trace_span_sync",
+    "Tracer",
+    "wrap",
+    "current_span_var",
+    "current_trace_var",
     "TraceManagerClient",
-    "JudgevalOTLPSpanExporter",
-    "JudgevalBatchSpanProcessor",
-    "create_trace_span_from_otel_span",
+    "JudgmentAPISpanExporter",
+    "JudgmentSpanProcessor",
 ]
