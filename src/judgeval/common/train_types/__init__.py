b4adcbaf3cc6cc16565b9e41b79905127767c6a2
Eric Yachbes <kryptocrashmh@gmail.com>
2025-07-18 14:38:53 -0700

    add init for types

---
+++ src/judgeval/common/train_types/__init__.py
@@ -0,0 +1,37 @@
+import os
+
+# Import peft (and transformers by extension) before unsloth to enable sleep mode
+if os.environ.get("IMPORT_PEFT", "0") == "1":
+    import peft  # type: ignore # noqa: F401
+
+# Import unsloth before transformers, peft, and trl to maximize Unsloth optimizations
+# NOTE: If we import peft before unsloth to enable sleep mode, a warning will be shown
+if os.environ.get("IMPORT_UNSLOTH", "0") == "1":
+    import unsloth  # type: ignore # noqa: F401
+
+if os.environ.get("IMPORT_PEFT", "0") == "1":
+    # torch.cuda.MemPool doesn't currently support expandable_segments which is used in sleep mode
+    conf = os.environ["PYTORCH_CUDA_ALLOC_CONF"].split(",")
+    if "expandable_segments:True" in conf:
+        conf.remove("expandable_segments:True")
+    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = ",".join(conf)
+
+from . import dev
+from .gather import gather_trajectories, gather_trajectory_groups
+from .model import Model, TrainableModel
+from .trajectories import Trajectory, TrajectoryGroup
+from .types import Messages, MessagesAndChoices, Tools, TrainConfig
+
+__all__ = [
+    "dev",
+    "gather_trajectories",
+    "gather_trajectory_groups",
+    "Messages",
+    "MessagesAndChoices",
+    "Tools",
+    "Model",
+    "TrainableModel",
+    "TrainConfig",
+    "Trajectory",
+    "TrajectoryGroup",
+]
