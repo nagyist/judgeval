30b997a4f02975943422630648b9d5aed9908c3e
Mandolaro <minhquangpham6@gmail.com>
2025-08-01 19:44:12 -0700

    use nest_asyncio

--- src/judgeval/utils/async_utils.py
+++ src/judgeval/utils/async_utils.py
@@ -1,8 +1,7 @@
 """Async utilities for judgeval."""
 
 import asyncio
-import concurrent.futures
-import os
+import nest_asyncio
 
 
 def safe_run_async(coro):
@@ -16,16 +15,9 @@ def safe_run_async(coro):
         The result of the coroutine
     """
     try:
-        # Try to get the running loop
         asyncio.get_running_loop()
-        # If we get here, there's already a loop running
-        # Run in a separate thread to avoid "asyncio.run() cannot be called from a running event loop"
-        max_workers = min(
-            32, (os.cpu_count() or 1) + 4
-        )  # Same as ThreadPoolExecutor default
-        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-            future = executor.submit(asyncio.run, coro)
-            return future.result()
+        nest_asyncio.apply()
+        return asyncio.run(coro)
     except RuntimeError:
         # No event loop is running, safe to use asyncio.run()
         return asyncio.run(coro)
