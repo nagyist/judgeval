9a773ff35938af76093c7564291be778ec43db66
Alan Zhang <97066812+alanzhang25@users.noreply.github.com>
2025-10-25 20:23:17 -0700

    dont throw (#614)

--- src/judgeval/utils/guards.py
+++ src/judgeval/utils/guards.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+from judgeval.logger import judgeval_logger
 
 if TYPE_CHECKING:
     from typing import TypeVar
@@ -8,24 +9,27 @@ if TYPE_CHECKING:
     T = TypeVar("T")
 
 
-def expect_exists(value: T | None, message: str) -> T:
-    if value is None:
-        raise ValueError(message)
+def expect_exists(value: T | None, message: str, default: T) -> T:
+    if not value:
+        judgeval_logger.error(message)
+        return default
 
     return value
 
 
-def expect_api_key(api_key: str | None) -> str:
+def expect_api_key(api_key: str | None) -> str | None:
     return expect_exists(
         api_key,
         "API Key is not set, please set JUDGMENT_API_KEY in the environment variables or pass it as `api_key`",
+        default=None,
     )
 
 
-def expect_organization_id(organization_id: str | None) -> str:
+def expect_organization_id(organization_id: str | None) -> str | None:
     return expect_exists(
         organization_id,
         "Organization ID is not set, please set JUDGMENT_ORG_ID in the environment variables or pass it as `organization_id`",
+        default=None,
     )
 
 
