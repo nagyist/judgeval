aa89fbc50e9462b4a0cf2c6849c182006b0b1e7d
Alan Zhang <97066812+alanzhang25@users.noreply.github.com>
2026-01-27 10:27:10 -0800

    Ahh/observe support for generators (#673)
    * format
    
    * add test
    
    * old naming
    
    * format
    
    ---------
    
    Co-authored-by: Abhishek Govindarasu <abhishekgovindarasu@gmail.com>

--- src/judgeval/v1/tracer/base_tracer.py
+++ src/judgeval/v1/tracer/base_tracer.py
@@ -1,13 +1,21 @@
 from __future__ import annotations
 
+import asyncio
+import contextvars
 import datetime
 import functools
 import inspect
 from abc import ABC, abstractmethod
+from collections.abc import (
+    Generator as ABCGenerator,
+    AsyncGenerator as ABCAsyncGenerator,
+)
 from contextlib import contextmanager
+from types import TracebackType
 from typing import (
     Any,
     Callable,
+    Coroutine,
     Dict,
     Iterator,
     Optional,
@@ -452,6 +460,7 @@ class BaseTracer(ABC):
         span_name: Optional[str] = None,
         record_input: bool = True,
         record_output: bool = True,
+        disable_generator_yield_span: bool = False,
     ) -> C: ...
 
     @overload
@@ -462,6 +471,7 @@ class BaseTracer(ABC):
         span_name: Optional[str] = None,
         record_input: bool = True,
         record_output: bool = True,
+        disable_generator_yield_span: bool = False,
     ) -> Callable[[C], C]: ...
 
     def observe(
@@ -471,10 +481,16 @@ class BaseTracer(ABC):
         span_name: Optional[str] = None,
         record_input: bool = True,
         record_output: bool = True,
+        disable_generator_yield_span: bool = False,
     ) -> C | Callable[[C], C]:
         if func is None:
             return lambda f: self.observe(
-                f, span_type, span_name, record_input, record_output
+                f,
+                span_type,
+                span_name,
+                record_input,
+                record_output,
+                disable_generator_yield_span,
             )  # type: ignore[return-value]
 
         if not self.enable_monitoring:
@@ -493,20 +509,19 @@ class BaseTracer(ABC):
                 with tracer.start_as_current_span(name) as span:
                     if span_type:
                         span.set_attribute(AttributeKeys.JUDGMENT_SPAN_KIND, span_type)
-
                     try:
                         if record_input:
-                            input_data = _format_inputs(func, args, kwargs)
                             span.set_attribute(
                                 AttributeKeys.JUDGMENT_INPUT,
-                                self.serializer(input_data),
+                                _serialize(
+                                    self.serializer, _format_inputs(func, args, kwargs)
+                                ),
                             )
-
                         result = await func(*args, **kwargs)
-
                         if record_output:
                             span.set_attribute(
-                                AttributeKeys.JUDGMENT_OUTPUT, self.serializer(result)
+                                AttributeKeys.JUDGMENT_OUTPUT,
+                                _serialize(self.serializer, result),
                             )
                         return result
                     except Exception as e:
@@ -514,36 +529,64 @@ class BaseTracer(ABC):
                         span.set_status(Status(StatusCode.ERROR, str(e)))
                         raise
 
-            return async_wrapper  # type: ignore[return-value]
+            return cast(C, async_wrapper)
         else:
 
             @functools.wraps(func)
             def sync_wrapper(*args: Any, **kwargs: Any) -> Any:
-                with tracer.start_as_current_span(name) as span:
+                with tracer.start_as_current_span(name, end_on_exit=False) as span:
                     if span_type:
                         span.set_attribute(AttributeKeys.JUDGMENT_SPAN_KIND, span_type)
-
                     try:
                         if record_input:
-                            input_data = _format_inputs(func, args, kwargs)
                             span.set_attribute(
                                 AttributeKeys.JUDGMENT_INPUT,
-                                self.serializer(input_data),
+                                _serialize(
+                                    self.serializer, _format_inputs(func, args, kwargs)
+                                ),
                             )
-
                         result = func(*args, **kwargs)
-
-                        if record_output:
-                            span.set_attribute(
-                                AttributeKeys.JUDGMENT_OUTPUT, self.serializer(result)
-                            )
-                        return result
                     except Exception as e:
                         span.record_exception(e)
                         span.set_status(Status(StatusCode.ERROR, str(e)))
+                        span.end()
                         raise
 
-            return sync_wrapper  # type: ignore[return-value]
+                    if inspect.isgenerator(result):
+                        if record_output:
+                            span.set_attribute(
+                                AttributeKeys.JUDGMENT_OUTPUT, "<generator>"
+                            )
+                        return _ObservedSyncGenerator(
+                            result,
+                            span,
+                            self.serializer,
+                            tracer,
+                            contextvars.copy_context(),
+                            disable_generator_yield_span or not record_output,
+                        )
+                    if inspect.isasyncgen(result):
+                        if record_output:
+                            span.set_attribute(
+                                AttributeKeys.JUDGMENT_OUTPUT, "<async_generator>"
+                            )
+                        return _ObservedAsyncGenerator(
+                            result,
+                            span,
+                            self.serializer,
+                            tracer,
+                            contextvars.copy_context(),
+                            disable_generator_yield_span or not record_output,
+                        )
+                    if record_output:
+                        span.set_attribute(
+                            AttributeKeys.JUDGMENT_OUTPUT,
+                            _serialize(self.serializer, result),
+                        )
+                    span.end()
+                    return result
+
+            return cast(C, sync_wrapper)
 
     @overload
     def agent(self, func: C, /, *, identifier: Optional[str] = None) -> C: ...
@@ -642,3 +685,222 @@ def _format_inputs(
         return inputs
     except Exception:
         return {}
+
+
+def _serialize(serializer: Callable[[Any], str], value: Any) -> Any:
+    return value if isinstance(value, (str, int, float, bool)) else serializer(value)
+
+
+R = TypeVar("R")
+
+
+class _ObservedGeneratorBase:
+    __slots__ = (
+        "_generator",
+        "_span",
+        "_serializer",
+        "_tracer",
+        "_context",
+        "_closed",
+        "_disable_generator_yield_span",
+    )
+
+    _generator: Any
+    _span: Span
+    _serializer: Callable[[Any], str]
+    _tracer: trace.Tracer
+    _context: contextvars.Context
+    _closed: bool
+    _disable_generator_yield_span: bool
+
+    def __init__(
+        self,
+        generator: Any,
+        span: Span,
+        serializer: Callable[[Any], str],
+        tracer: trace.Tracer,
+        context: contextvars.Context,
+        disable_generator_yield_span: bool = False,
+    ) -> None:
+        self._generator = generator
+        self._span = span
+        self._serializer = serializer
+        self._tracer = tracer
+        self._context = context
+        self._closed = False
+        self._disable_generator_yield_span = disable_generator_yield_span
+
+    def _emit_yield_span(self, item: Any) -> None:
+        if self._disable_generator_yield_span:
+            return
+        with trace.use_span(self._span):
+            span_name = str(getattr(self._span, "name", "generator_item"))
+            with self._tracer.start_as_current_span(
+                span_name,
+                attributes={AttributeKeys.JUDGMENT_SPAN_KIND: "generator_item"},
+            ) as child_span:
+                child_span.set_attribute(
+                    AttributeKeys.JUDGMENT_OUTPUT,
+                    _serialize(self._serializer, item),
+                )
+
+    def _record_error(self, exc: BaseException) -> None:
+        self._span.record_exception(exc)
+        self._span.set_status(Status(StatusCode.ERROR, str(exc)))
+        self._finish()
+
+    def _finish(self) -> None:
+        if self._closed:
+            return
+        self._closed = True
+        self._span.set_attribute(AttributeKeys.JUDGMENT_SPAN_KIND, "generator")
+        self._span.end()
+
+    def __del__(self) -> None:
+        self._finish()
+
+
+class _ObservedSyncGenerator(_ObservedGeneratorBase, ABCGenerator[Any, Any, Any]):
+    __slots__ = ()
+
+    def __iter__(self) -> _ObservedSyncGenerator:
+        return self
+
+    def __next__(self) -> Any:
+        return self.send(None)
+
+    def send(self, value: Any) -> Any:
+        if self._closed:
+            raise StopIteration
+        try:
+            item = self._context.run(self._generator.send, value)
+            self._emit_yield_span(item)
+            return item
+        except StopIteration:
+            self._finish()
+            raise
+        except Exception as e:
+            self._record_error(e)
+            raise
+
+    @overload
+    def throw(
+        self,
+        __typ: type[BaseException],
+        __val: object = ...,
+        __tb: Optional[TracebackType] = ...,
+    ) -> Any: ...
+
+    @overload
+    def throw(
+        self,
+        __typ: BaseException,
+        __val: None = ...,
+        __tb: Optional[TracebackType] = ...,
+    ) -> Any: ...
+
+    def throw(
+        self,
+        __typ: type[BaseException] | BaseException,
+        __val: object = None,
+        __tb: Optional[TracebackType] = None,
+    ) -> Any:
+        if self._closed:
+            raise StopIteration
+        try:
+            if isinstance(__typ, type):
+                item = self._context.run(self._generator.throw, __typ, __val, __tb)
+            else:
+                item = self._context.run(self._generator.throw, __typ, None, __tb)
+            self._emit_yield_span(item)
+            return item
+        except StopIteration:
+            self._finish()
+            raise
+        except Exception as e:
+            self._record_error(e)
+            raise
+
+    def close(self) -> None:
+        try:
+            self._generator.close()
+        finally:
+            self._finish()
+
+
+class _ObservedAsyncGenerator(_ObservedGeneratorBase, ABCAsyncGenerator[Any, Any]):
+    __slots__ = ()
+
+    def _create_task(self, coro: Coroutine[Any, Any, R]) -> asyncio.Task[R]:
+        try:
+            return asyncio.create_task(coro, context=self._context)
+        except TypeError:
+            return self._context.run(lambda: asyncio.create_task(coro))
+
+    def __aiter__(self) -> _ObservedAsyncGenerator:
+        return self
+
+    async def __anext__(self) -> Any:
+        return await self.asend(None)
+
+    async def asend(self, value: Any) -> Any:
+        if self._closed:
+            raise StopAsyncIteration
+        try:
+            item = await self._create_task(self._generator.asend(value))
+            self._emit_yield_span(item)
+            return item
+        except StopAsyncIteration:
+            self._finish()
+            raise
+        except Exception as e:
+            self._record_error(e)
+            raise
+
+    @overload
+    async def athrow(
+        self,
+        __typ: type[BaseException],
+        __val: object = ...,
+        __tb: Optional[TracebackType] = ...,
+    ) -> Any: ...
+
+    @overload
+    async def athrow(
+        self,
+        __typ: BaseException,
+        __val: None = ...,
+        __tb: Optional[TracebackType] = ...,
+    ) -> Any: ...
+
+    async def athrow(
+        self,
+        __typ: type[BaseException] | BaseException,
+        __val: object = None,
+        __tb: Optional[TracebackType] = None,
+    ) -> Any:
+        if self._closed:
+            raise StopAsyncIteration
+        try:
+            if isinstance(__typ, type):
+                item = await self._create_task(
+                    self._generator.athrow(__typ, __val, __tb)
+                )
+            else:
+                item = await self._create_task(
+                    self._generator.athrow(__typ, None, __tb)
+                )
+            self._emit_yield_span(item)
+            return item
+        except StopAsyncIteration:
+            self._finish()
+            raise
+        except Exception as e:
+            self._record_error(e)
+            raise
+
+    async def aclose(self) -> None:
+        try:
+            await self._generator.aclose()
+        finally:
+            self._finish()
